c658c8bfb8651f106675b611b82a472d83a4bb11
smbn <93800484+smbn@users.noreply.github.com>
2024-07-01 15:21:02 +0000

    initial commit

---
+++ src/app/components/upload-file/upload-file/upload-file.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-file',
+  templateUrl: './upload-file.component.html',
+  styleUrl: './upload-file.component.css'
+})
+export class UploadFileComponent {
+
+}
